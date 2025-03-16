684b6bcab9735431db2e1af62a68844ce002b80c
Abhi Yerra <abhi@opszero.com>
2025-03-15 17:04:30 -0700

    Added

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -69,18 +69,18 @@ const config: Config = {
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
       title: 'Jnana Press',
-      logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo.svg',
-      },
+      // logo: {
+      //   alt: 'My Site Logo',
+      //   src: 'img/logo.svg',
+      // },
       items: [
-        {
-          type: 'docSidebar',
-          sidebarId: 'tutorialSidebar',
-          position: 'left',
-          label: 'Tutorial',
-        },
-        { to: '/blog', label: 'Blog', position: 'left' },
+        // {
+        //   type: 'docSidebar',
+        //   sidebarId: 'jnanaSidebar',
+        //   position: 'left',
+        //   label: 'Tutorial',
+        // },
+        // { to: '/blog', label: 'Blog', position: 'left' },
         {
           href: 'https://github.com/JnanaPress/JnanaPress',
           label: 'GitHub',
@@ -92,41 +92,41 @@ const config: Config = {
       style: 'dark',
       links: [
         {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Tutorial',
-              to: '/docs/intro',
-            },
-          ],
+          // title: 'Docs',
+          // items: [
+          //   {
+          //     label: 'Tutorial',
+          //     to: '/docs/intro',
+          //   },
+          // ],
         },
         {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'X',
-              href: 'https://x.com/docusaurus',
-            },
-          ],
+          // title: 'Community',
+          // items: [
+          //   {
+          //     label: 'Stack Overflow',
+          //     href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+          //   },
+          //   {
+          //     label: 'Discord',
+          //     href: 'https://discordapp.com/invite/docusaurus',
+          //   },
+          //   {
+          //     label: 'X',
+          //     href: 'https://x.com/docusaurus',
+          //   },
+          // ],
         },
         {
           title: 'More',
           items: [
             {
-              label: 'Blog',
-              to: '/blog',
+              label: 'Sponsored by opsZero',
+              href: 'https://opszero.com',
             },
             {
               label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
+              href: 'https://github.com/JnanaPress/JnanaPress',
             },
           ],
         },
